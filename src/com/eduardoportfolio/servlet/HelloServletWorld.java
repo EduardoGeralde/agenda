e96a2d12dec70e9eb32ba54ac7cc21c2f04619cc
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-02 16:54:41 -0200

    Annotation @WebServlet Inserted

--- src/com/eduardoportfolio/servlet/HelloServletWorld.java
+++ src/com/eduardoportfolio/servlet/HelloServletWorld.java
@@ -10,23 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/helloServlet")
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello","/holla"})
 public class HelloServletWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
+			throws ServletException ,IOException {
 		
 		PrintWriter out = response.getWriter();
 		
 		out.println("<html>");
 		out.println("<head>");
-		out.println("<title>First Servlet</tilte>");
+		out.println("<title>First Servlet</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("<h1>Hello servlet's world ! </h1>");
+		out.println("<h1>Hello servlet world ! </h1>");
 		out.println("</body>");
 		out.println("</html>");
 	}
