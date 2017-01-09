9a53418f75283d67fbd3b9b629cb2eef2717ad27
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-09 13:29:15 -0200

    logic to list contact created

---
+++ src/com/eduardoportfolio/mvc/logic/ContactListLogic.java
@@ -0,0 +1,23 @@
+package com.eduardoportfolio.mvc.logic;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.eduardoportfolio.jdbc.dao.ContactDao;
+import com.eduardoportfolio.jdbc.model.Contact;
+
+public class ContactListLogic implements Logic {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		List<Contact> contacts = new ContactDao().getList();
+		
+		request.setAttribute("contacts", contacts);
+		
+		return "contactListJSTL.jsp";
+	}
+
+}
