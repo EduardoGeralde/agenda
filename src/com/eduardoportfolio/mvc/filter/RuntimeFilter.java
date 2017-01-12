c4d340dc5a67c22f688ee9bc19abfa40ca8509cb
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-11 22:44:27 -0200

    improvements and comments

--- src/com/eduardoportfolio/mvc/filter/RuntimeFilter.java
+++ src/com/eduardoportfolio/mvc/filter/RuntimeFilter.java
@@ -11,35 +11,53 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
+
+/**
+ * @author Eduardo Geralde Neto
+ * 
+ * This class is a filter, which all requisitions ("/*") of our application has to be filtered. This helps us not to 
+ * need to put this logic in all our classes. In this specific case, this filter takes the current time before the 
+ * logic/resource, and the time on returning way, when close this logic/resource, then, prints the
+ * traced result in the console. Filters are good ways to errors handling, measure execution time, close 
+ * doors, etc...
+ */
+
 @WebFilter("/*")
 public class RuntimeFilter implements Filter {
-
-	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
+		//Takes the initial time
 		long initialTime = System.currentTimeMillis();
 		
+		//Point thats mark our execution period
 		chain.doFilter(request, response);
 		
+		//Takes the final time, when the operation was done
 		long finalTime = System.currentTimeMillis();
 		
+		//Parameters to trace the logic/resource
 		String uri = ((HttpServletRequest)request).getRequestURI();
 		String parameters = ((HttpServletRequest)request).getParameter("logic");
 		
+		//Printing results
 		System.out.print("Requisition time for ");
                 System.out.print(uri + "?logic= ");
                 System.out.print(parameters + " took ");
                 System.out.println(finalTime-initialTime + " ms");
 	}
 
+	//This method is to execute something when this filter is loaded by the container
 	@Override
 	public void init(FilterConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	
+	//This method is to execute something when this filter is unloaded by the container
+	@Override
+	public void destroy() {
 		// TODO Auto-generated method stub
 	}
 
