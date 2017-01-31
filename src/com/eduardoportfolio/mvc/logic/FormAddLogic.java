10448fca1e2f64bde4d7ff8352ddb3c360fe9a56
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-01-30 23:34:08 -0200

    improved

--- src/com/eduardoportfolio/mvc/logic/FormAddLogic.java
+++ src/com/eduardoportfolio/mvc/logic/FormAddLogic.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author Eduardo Geralde Neto
  * 
  * The logic of add a contact, in fact, is coupled with the update's logic in another logic class. This class
- * only returns, as a String, where we want to be dispatched, to the Controller.
+ * only returns, as a String, where we want to be dispatched, to the Controller
  */
 
 public class FormAddLogic implements Logic {
