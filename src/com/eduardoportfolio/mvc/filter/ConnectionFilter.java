7919e6fadb061dcb2af1416b3c793c75c0742092
Eduardo Geralde Neto <eduardogeralde83@gmail.com>
2017-03-04 19:04:13 -0300

    comments

--- src/com/eduardoportfolio/mvc/filter/ConnectionFilter.java
+++ src/com/eduardoportfolio/mvc/filter/ConnectionFilter.java
@@ -17,7 +17,7 @@ import com.eduardoportfolio.jdbc.ConnectionFactory;
  * 
  * This class is a filter, which provides a connection for each requisition, and just close this connection at 
  * the end (return) of this resource. Inversion of control, Filter created for dependency injection 
- * (connection) in DAO
+ * (connection) in DAO.
  */
 
 @WebFilter("/*")
